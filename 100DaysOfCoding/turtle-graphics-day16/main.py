af033caddc46e1a947351e294f43af4488414c87
Moazzam Adil Khan <moaadi3890@gmail.com>
2021-01-18 10:36:02 +0500

    Installing libraries of pypi-Python Package Index

--- 100DaysOfCoding/turtle-graphics-day16/main.py
+++ 100DaysOfCoding/turtle-graphics-day16/main.py
@@ -1,16 +1,42 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# champu = Turtle()
+#
+# champu.shape("turtle")
+#
+# champu.color("DarkOrange", "brown")
+# champu.position()
+# champu.fd(100)
+# champu.setheading(75)
+# champu.fd(50)
+#
+#
+# turtle_screen = Screen()
+# turtle_screen.exitonclick()
+# turtle_screen.title("Welcome to Turtle Cage.")
 
-champu = Turtle()
+from prettytable import PrettyTable
+table_object = PrettyTable()
+table_object.add_column("Pokemon Name",
+                        ["Pikachu", "Squirtle", "Charmandor"])
+table_object.add_column("Type",
+                        ["Electric", "Water", "Fire"])
+table_object.align = "l"
+print(table_object)
 
-champu.shape("turtle")
 
-champu.color("DarkOrange", "brown")
-champu.position()
-champu.fd(100)
-champu.setheading(75)
-champu.fd(50)
+table_object.clear()
 
 
-turtle_screen = Screen()
-turtle_screen.exitonclick()
-turtle_screen.title("Welcome to Turtle Cage.")
+table_object.field_names = ["Pokemon Name", "Type"]
+table_object.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Charmandor", "Fire"],
+    ]
+)
+table_object.align["Pokemon Name"] = "l"
+print(table_object)
+
+
