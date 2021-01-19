91b3727c0e1c09d2935d9b317657cf47d2cfd2da
Moazzam Adil Khan <moaadi3890@gmail.com>
2021-01-19 11:15:32 +0500

    Coffee Machine with OOP completed.

--- 100DaysOfCoding/oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ 100DaysOfCoding/oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -1,4 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+m = Menu()
+cm = CoffeeMaker()
+mm = MoneyMachine()
+
+turn_off = False
+while not turn_off:
+    user_selection = input(f"What would you like to have? {m.get_items()}:")
+
+    if user_selection == "off":
+        print("Machine is turning OFF. BYE BYE...")
+        turn_off = True
+    elif user_selection == "report":
+        cm.report()
+        mm.report()
+    else:
+        item = m.find_drink(user_selection)
+        if item is None:
+            print("We have only three options available.")
+        can_make = cm.is_resource_sufficient(item)
+        if can_make is True:
+            mm.make_payment(item.cost)
+            cm.make_coffee(item)
+
+
 
